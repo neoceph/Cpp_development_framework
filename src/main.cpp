046bc275ecccbde319ac7b568ffe0e8b5fea9dc6
nirjhor <nirjhor18@gmail.com>
2022-03-13 10:19:21 -0400

    git versioning into CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 
+#include "version.h"
+
 using namespace std;
 
 int main ()
 {
     cout << "Hello World!\n";
+    std::cout << gGIT_VERSION << std::endl << gGIT_VERSION_SHORT << std::endl;
     
 }

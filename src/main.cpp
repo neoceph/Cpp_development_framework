dd4dd10f4ab9f036f0bdee2a0ee85f631fae913a
Abdullah Al Amin <nirjhor18@gmail.com>
2023-02-12 19:13:58 -0600

    added example vectors, lists, and .gdbinit for debugging.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include <vector>
+#include <list>
+#include <map>
 
 #include "version.h"
 
@@ -6,8 +9,35 @@ using namespace std;
 
 int main (int arg, char *argv[])
 {
-    int array[3] = {0, 1, 2};
-    cout << "Hello World!\n";
-    std::cout << gGIT_VERSION << std::endl << gGIT_VERSION_SHORT << std::endl;
+    VersionInfo version;
+
+    std::cout << "Cpp development framework!\n";
+
+
+    if (arg != 2)
+    {
+        
+        std::cout << "Missing parameter: input file name" << std::endl;
+        exit(1);
+      return 0;
+    }
+
+    else
+    {
+        version.getVersionInfo(argv[1]);
+    }
+
+    std::string s {"Test String"};
+    std::vector<double> v (3, 3.14);
+    std::list<int> l;
+
+    l.push_back(3);
+    l.push_back(1);
+    l.push_back(3);
+
+
+    std::map<int, int> testMap;
+    testMap[7] = 2;
+    testMap[2] = -987;
     
 }
